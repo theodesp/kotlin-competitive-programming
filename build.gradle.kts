be3090c82404214c64e936664a8f702e3ea45ed9
Theo <theo.despoudis@teckro.com>
2019-11-25 16:00:45 +0000

    Setting up Spek with test logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     kotlin("jvm") version "1.3.60"
@@ -18,9 +20,47 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("junit:junit:4.12")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.8")
+    testImplementation("org.hamcrest:hamcrest-library:1.3")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.8")
+    testRuntimeOnly(kotlin("reflect"))
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform {
+        includeEngines("spek2")
+    }
+    testLogging {
+        lifecycle {
+            events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+            exceptionFormat = TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+            showStandardStreams = true
+        }
+        info.events = lifecycle.events
+        info.exceptionFormat = lifecycle.exceptionFormat
+    }
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) { // root suite
+                logger.info("----")
+                logger.info("Test result: ${result.resultType}")
+                logger.info("Test summary: ${result.testCount} tests, " +
+                        "${result.successfulTestCount} succeeded, " +
+                        "${result.failedTestCount} failed, " +
+                        "${result.skippedTestCount} skipped")
+
+            }
+        }
+    })
 }
--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,11 +1,11 @@
-package org.example
-
-import org.junit.Assert.assertEquals
-import org.junit.*
-
-class MyLibraryTest {
-    @Test
-    fun testMyLanguage() {
-        assertEquals("Kotlin", "Kotlin")
-    }
-}
+//package org.example
+//
+//import org.junit.Assert.assertEquals
+//import org.junit.*
+//
+//class MyLibraryTest {
+//    @Test
+//    fun testMyLanguage() {
+//        assertEquals("Kotlin", "Kotlin")
+//    }
+//}
